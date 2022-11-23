ebefd3b6a798c89bf813e85a81f8b320050e1f89
robinsippel <robin.sippel@gmail.com>
2022-11-23 09:24:32 +0100

    update all

--- main.py
+++ main.py
@@ -35,6 +35,7 @@ START: pip install and import required libraries
 import pandas as pd
 import sqlalchemy as db
 import sqlite3
+import csv
 import matplotlib.pyplot as plt
 
 """
@@ -42,9 +43,9 @@ GET csv data into python objects. Datasetes should be stored in program folder.
 """
 
 # Reading data from csv files using pandas
-data_train = pd.read_csv('Datensatz/train.csv')
-data_test = pd.read_csv('Datensatz/test.csv')
-data_ideal = pd.read_csv('Datensatz/ideal.csv')
+# data_train = pd.read_csv('Datensatz/train.csv')
+# data_test = pd.read_csv('Datensatz/test.csv')
+# data_ideal = pd.read_csv('Datensatz/ideal.csv')
 
 """
 CREATE an sqlite database in project folder /
@@ -52,31 +53,51 @@ With the following code we connect th program to the database and create sqlite 
 from data_train and data_ideal based on csv files previously loaded
 """
 
-# Connect an SQLite Database
-con_str = "sqlite:///Hausarbeit.db"  # link to created database
-engine = db.create_engine(con_str)
-connection = engine.connect()
-
-# Create Tables based on CSV files
-def create_sql(csv_data, sql_tablename):
-
+class SQL_Data():
     """
-    Enter the 'csv_data' object and the 'sql_tablename' to transform the one in the other
-    The exception handling ensure that the program gets executed although a table with entered name already exists.
+    This class controlles the creation, access and update of sqlite tables
     """
 
-    try:
-        csv_data.to_sql(sql_tablename, engine, index=False) == ValueError
-        pass
-    except:
-        print(f"sql table with name '{sql_tablename}' already exist and will be not created again")
-        pass
-    finally:
-        pass
+    # connect database using sqlalchemy for initial table creation from csv
+    con_str = "sqlite:///Hausarbeit.db"
+    engine = db.create_engine(con_str)
+    connection = engine.connect()
+    # connect database using sqlite3 to modify existing sql tables
+    con = sqlite3.connect("Hausarbeit.db")
+    cur = con.cursor()
+    def create_sql_table_from_csv(self, csv_path, sql_tablename):
+
+        """
+        Enter the 'csv_path'(string) object and the new 'sql_tablename'(string) to transform the one into the other
+        The exception handling ensure that the program gets executed although a table with entered name already exists.
+        """
+        csv_data = pd.read_csv(f'{csv_path}')
+        try:
+            csv_data.to_sql(sql_tablename, self.engine, index=False, if_exists="fail") == ValueError
+            pass
+        except:
+            print(f"sql table with name '{sql_tablename}' already exist and will be not be replaced again")
+            pass
+        finally:
+            pass
+
+    def add_table_column(self, table_name, new_column_name):
+        """
+        with this funktion new columns can be added to an existing sqlite database
+        :param column_name:
+        :return:
+        """
+        self.cur.execute(f"ALTER TABLE {table_name} ADD {new_column_name}")
 
 # Create sqlite database by entering existing data object and to be created table name as string
-create_sql(data_train, "train")
-create_sql(data_ideal, "ideal")
+data = SQL_Data()
+data.create_sql_table_from_csv(csv_path="Datensatz/train.csv", sql_tablename="train")
+data.create_sql_table_from_csv(csv_path="Datensatz/ideal.csv", sql_tablename="ideal")
+data.create_sql_table_from_csv(csv_path="Datensatz/test.csv", sql_tablename="test")
+
+# Add new column to test table that did not exist before, Exception handling required
+data.add_table_column(table_name="test", new_column_name="Delta Y")
+data.add_table_column(table_name="test", new_column_name="Nummer der Idealen Funktion")
 
 """
 COMPUTE_1: identify the four ideal function from data_ideal that fit best to data_train / 
