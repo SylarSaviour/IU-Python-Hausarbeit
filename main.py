fc34a36032fd83e6f7617af5cf9a5de5b4e03431
robinsippel <robin.sippel@gmail.com>
2022-11-24 19:58:16 +0100

    function to update and select sql values in SQL_Data class created

--- main.py
+++ main.py
@@ -48,25 +48,26 @@ GET csv data into python objects. Datasetes should be stored in program folder.
 # data_ideal = pd.read_csv('Datensatz/ideal.csv')
 
 """
-CREATE an sqlite database in project folder /
-With the following code we connect th program to the database and create sqlite tables /
-from data_train and data_ideal based on csv files previously loaded
+With the following code we create an sqlite database in project folder / 
+connect th program to the database and create sqlite tables /
 """
+# create inital sqlite database in project folder
+sqlite3.connect("Hausarbeit.db")
 
 class SQL_Data():
     """
     This class controlles the creation, access and update of sqlite tables
     """
 
+    # connect database using sqlite3 to modify existing sql tables
+    con = sqlite3.connect("Hausarbeit.db")
+    cur = con.cursor()
+
     # connect database using sqlalchemy for initial table creation from csv
     con_str = "sqlite:///Hausarbeit.db"
     engine = db.create_engine(con_str)
     connection = engine.connect()
-    # connect database using sqlite3 to modify existing sql tables
-    con = sqlite3.connect("Hausarbeit.db")
-    cur = con.cursor()
     def create_sql_table_from_csv(self, csv_path, sql_tablename):
-
         """
         Enter the 'csv_path'(string) object and the new 'sql_tablename'(string) to transform the one into the other
         The exception handling ensure that the program gets executed although a table with entered name already exists.
@@ -76,7 +77,7 @@ class SQL_Data():
             csv_data.to_sql(sql_tablename, self.engine, index=False, if_exists="fail") == ValueError
             pass
         except:
-            print(f"sql table with name '{sql_tablename}' already exist and will be not be replaced again")
+            print(f"sql table with name '{sql_tablename}' already exist and will not be replaced")
             pass
         finally:
             pass
@@ -87,7 +88,38 @@ class SQL_Data():
         :param column_name:
         :return:
         """
-        self.cur.execute(f"ALTER TABLE {table_name} ADD {new_column_name}")
+        try:
+            self.cur.execute(f"ALTER TABLE {table_name} ADD {new_column_name}")
+            pass
+        except:
+            print(f"new column '{new_column_name}' in table '{table_name}' already exist and will not be replaced")
+            pass
+        finally:
+            pass
+
+    def update_values(self, table_name, column_name_entry, value = None,
+                      column_name_x = 'x', column_value_x = None,
+                      column_name_y ='y', column_value_y = None):
+        """
+        with this funktion data can be inserted in a column of a sqlite database table
+        :param column_name:
+        :return:
+        """
+        self.cur.execute(f"UPDATE {table_name} SET {column_name_entry}={value} WHERE {column_name_x}={column_value_x} AND {column_name_y}={column_value_y}")
+        self.con.commit()
+
+    def select_values(self, table_name, column_name, value):
+        """
+        with this funktion data from a databse can be selected
+        :param column_name:
+        :return:
+        """
+        self.cur.execute(f"SELECT * FROM {table_name} WHERE {column_name}={value}")
+        result = self.cur.fetchall()
+
+        for row in result:
+            print(row)
+
 
 # Create sqlite database by entering existing data object and to be created table name as string
 data = SQL_Data()
@@ -96,8 +128,23 @@ data.create_sql_table_from_csv(csv_path="Datensatz/ideal.csv", sql_tablename="id
 data.create_sql_table_from_csv(csv_path="Datensatz/test.csv", sql_tablename="test")
 
 # Add new column to test table that did not exist before, Exception handling required
-data.add_table_column(table_name="test", new_column_name="Delta Y")
-data.add_table_column(table_name="test", new_column_name="Nummer der Idealen Funktion")
+data.add_table_column(table_name="test", new_column_name="Delta_Y")
+data.add_table_column(table_name="test", new_column_name="Nummer_der_Idealen_Funktion")
+
+
+# Test the data update function
+# data.update_values(table_name="test",
+#                    column_name_entry="Nummer_der_Idealen_Funktion",
+#                    value=1,
+#                    column_value_x=-11.1,
+#                    column_value_y=-11.129228)
+
+#Test data selection
+# data.select_values(table_name="test", column_name="Nummer_der_Idealen_Funktion", value=1)
+
+
+
+
 
 """
 COMPUTE_1: identify the four ideal function from data_ideal that fit best to data_train / 
